3e229eec73b4b76de696478f85ca40a2f5cb132f
kimziaco <88760828+kimziaco@users.noreply.github.com>
2021-09-15 11:26:28 +0900

    백준공부

---
+++ 백준공부/1929.py
@@ -0,0 +1,16 @@
+import math
+
+M, N = map(int, input().split())
+
+arry = [True for i in range(N+1)]
+
+for i in range(2, int(math.sqrt(N))+1):
+    if arry[i] == True:
+        j = 2
+        while i*j <= N:
+            arry[i*j] = False
+            j += 1
+
+for i in range(M, N+1):
+    if arry[i]:
+        print(i)
